fd7c1a7ef0a941c9ea1dfe602e2cb3e986608e4e
Kvisus <eternal.wanderer.z@gmail.com>
2023-12-21 20:37:53 +0300

    separate admin\shop microfrontends

--- services/host/src/index.tsx
+++ services/host/src/index.tsx
@@ -1,22 +1,2 @@
-import { createRoot } from "react-dom/client";
-import { App } from "./components/App";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import React, { Suspense } from "react";
-
-const root = document.getElementById("root");
-
-if (!root) {
-  throw new Error("root not found");
-}
-
-const container = createRoot(root);
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [],
-  },
-]);
-
-container.render(<RouterProvider router={router} />);
+import("./bootstrap");
+export {};
